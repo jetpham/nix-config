863a046f423eb853e3138db5fe7467791f2b2823
Jet Pham <jetthomaspham@gmail.com>
2025-08-15 18:24:14 -0700

    feat: init flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "flake for jet with Home Manager enabled";
+  # https://github.com/drakerossman/nixos-musings/blob/main/how-to-add-home-manager-to-nixos/flake.nix
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  }: {
+    nixosConfigurations = {
+      jet = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.jet = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,131 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  home.username = "jet";
+  home.stateVersion = "23.05";
+
+  home.packages = with pkgs; [
+    code-cursor
+    ghidra-bin
+    kitty
+    zellij
+    jujutsu
+    vlc
+    docker
+    btop
+    inputs.zen-browser.packages."${pkgs.system}".twilight-official
+    nerd-fonts.commit-mono
+    prismlauncher
+    steam
+    qbittorrent-enhanced
+    openexr # for omelia
+    gimp3
+    obs-studio
+    inkscape
+    blender
+    kdePackages.kdenlive
+    android-studio
+    bat
+    zoxide
+    eza
+    ripgrep
+  ];
+
+  programs.eza = {
+    enable = true;
+    icons = "always";
+    enableBashIntegration = true;
+    git = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      ll = "eza -l";
+      la = "eza -la";
+      ".." = "cd ..";
+      gs = "git status";
+      gc = "git commit";
+      gp = "git push";
+      gl = "git pull";
+      cat = "bat";
+      grep = "rg --color=auto";
+      cls = "clear";
+      j = "jj";
+      jgf = "jj git fetch";
+      jgp = "jj git push";
+      jgc = "jj git clone --colocate";
+      jd = "jj describe";
+    };
+  };
+
+  programs.kitty = {
+    enable = true;
+    settings = {
+      hide_window_decorations = "yes";
+      draw_minimal_borders = "yes";
+      font_family = "CommitMono Nerd Font";
+      font_size = "12";
+      confirm_os_window_close = "0";
+      enable_audio_bell = "no";
+    };
+    keybindings = {
+      "ctrl+shift+c" = "copy_and_clear_or_interrupt";
+      "ctrl+c" = "copy_and_clear_or_interrupt";
+      "ctrl+shift+v" = "paste_from_clipboard";
+      "ctrl+v" = "paste_from_clipboard";
+    };
+    themeFile = "GitHub_Dark_High_Contrast";
+  };
+
+  programs.jujutsu = {
+    enable = true;
+    settings = {
+      user = {
+        email = "jetthomaspham@gmail.com";
+        name = "Jet Pham";
+      };
+
+      signing = {
+        behavior = "own";
+        backend = "ssh";
+        key = "~/.ssh/id_ed25519.pub";
+      };
+
+      git = {
+        sign-on-push = true;
+        push-new-bookmarks = true;
+      };
+      ui = {
+        editor = "hx";
+        pager = "bat";
+      };
+    };
+  };
+
+  # Override the Kitty desktop entry to always launch in fullscreen
+  xdg.desktopEntries.kitty = {
+    name = "Kitty";
+    genericName = "Terminal Emulator";
+    exec = "kitty --start-as=fullscreen";
+    icon = "kitty";
+    type = "Application";
+    categories = ["System" "TerminalEmulator"];
+    comment = "Fast, featureful, GPU based terminal emulator";
+  };
+  
+  # Autostart applications using proper desktop files
+  xdg.autostart = {
+    enable = true;
+    entries = [
+      "${pkgs.kitty}/share/applications/kitty.desktop"
+      "${inputs.zen-browser.packages."${pkgs.system}".twilight-official}/share/applications/zen-twilight.desktop"
+      "${pkgs.code-cursor}/share/applications/cursor.desktop"
+    ];
+  };
+}
