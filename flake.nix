f0a1bf8e76c0315aa4e4a14262122fbb71921a5e
Jet Pham <jetthomaspham@gmail.com>
2026-02-06 03:48:47 +0100

    fix: add backup postfix

--- flake.nix
+++ flake.nix
@@ -29,6 +29,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
             home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.jet = import ./home.nix;
           }
