4ac1fd148994ee6b811d4cc2f294a8150f61910e
Jet Pham <jetthomaspham@gmail.com>
2026-03-08 19:04:38 -0700

    fix: remove default languages already set

--- configuration.nix
+++ configuration.nix
@@ -33,18 +33,6 @@
   time.timeZone = "America/Los_Angeles";
   i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
   services.xserver.enable = true;
 
   # Framework Laptop 13 AMD AI 300 Series specific configurations
