00462e4d0cbb43b674c7734d0dd08a59f0f59772
Jet <jet@extremist.software>
2026-05-05 18:10:37 -0700

    feat: disable top bar in zen

--- home-modules/browser.nix
+++ home-modules/browser.nix
@@ -36,10 +36,47 @@
       isDefault = true;
       settings = {
         "identity.fxaccounts.enabled" = false;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "ui.prefersReducedMotion" = 1;
         "zen.theme.border-radius" = 0;
         "zen.theme.content-element-separation" = 0;
+        "zen.view.compact.show-sidebar-and-toolbar-on-hover" = true;
       };
+      userChrome = ''
+        @-moz-document url("chrome://browser/content/browser.xhtml") {
+          @media -moz-pref("zen.view.compact.hide-toolbar") {
+            :root[zen-compact-mode="true"]:not([customizing]):not([inDOMFullscreen="true"]):not([zen-single-toolbar="true"])
+              #zen-appcontent-navbar-wrapper:not([has-popup-menu]):not([zen-compact-mode-active]) {
+              pointer-events: none !important;
+            }
+
+            :root[zen-compact-mode="true"]:not([customizing]):not([inDOMFullscreen="true"]):not([zen-single-toolbar="true"])
+              #zen-appcontent-navbar-wrapper[zen-has-hover]:not([has-popup-menu]):not([zen-compact-mode-active]) {
+              height: var(--zen-element-separation) !important;
+              overflow: clip !important;
+            }
+
+            :root[zen-compact-mode="true"]:not([customizing]):not([inDOMFullscreen="true"]):not([zen-single-toolbar="true"])
+              #zen-appcontent-navbar-wrapper[zen-has-hover]:not([has-popup-menu]):not([zen-compact-mode-active])
+              #urlbar:not([breakout-extend="true"]) {
+              opacity: 0 !important;
+              pointer-events: none !important;
+            }
+
+            :root[zen-compact-mode="true"]:not([customizing]):not([inDOMFullscreen="true"]):not([zen-single-toolbar="true"])
+              #zen-appcontent-navbar-wrapper[zen-has-hover]:not([has-popup-menu]):not([zen-compact-mode-active])
+              #zen-appcontent-navbar-container {
+              opacity: 0 !important;
+            }
+
+            :root[zen-compact-mode="true"]:not([customizing]):not([inDOMFullscreen="true"]):not([zen-single-toolbar="true"])
+              #zen-appcontent-navbar-wrapper[zen-has-hover]:not([has-popup-menu]):not([zen-compact-mode-active])
+              .titlebar-buttonbox-container {
+              max-height: 0 !important;
+            }
+          }
+        }
+      '';
       extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
         ublock-origin
         onepassword-password-manager
