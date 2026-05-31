93483115c9034c55521ac784435111fe6f598c42
Jet <jet@extremist.software>
2026-05-31 11:47:46 -0700

    Update framework work SSH key

--- ssh-public-keys.nix
+++ ssh-public-keys.nix
@@ -1,6 +1,6 @@
 {
   jet = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE40ISu3ydCqfdpb26JYD5cIN0Fu0id/FDS+xjB5zpqu jet@extremist.software"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPyic30I+SaDw0Lz/EFpMNeHCwxpwPfkgfR6uz3g7io7 jet@corp.primitive.dev"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFDt33yZLLdyH0MTU/iXcK3Y70vbJD9X97vEMTfu272F jet@corp.primitive.dev"
   ];
 }
